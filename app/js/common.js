88d86e21ed7a1253bcbbd05edd87af9a75a2718d
Viktoria <zaharova181114@gmail.com>
2021-04-25 12:29:23 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,20 @@
+// активная ссылка меню
+$('.menu li a').each(function () {
+    var location = window.location.href;
+    var link = this.href;
+    if (location === link) {
+        $(this).addClass('active');
+    }
+});
+// end
+
+// mobile menu
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+$('[data-toggle="tooltip"]').tooltip();
