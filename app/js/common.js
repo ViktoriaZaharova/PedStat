4b2f88b7d18c59202f9de26cf894c440fa623d03
Viktoria <zaharova181114@gmail.com>
2022-01-26 17:34:13 +0200

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -10,7 +10,7 @@ $('.menu li a').each(function () {
 
 // mobile menu
 $('.btn-burger').on('click', function () {
-   $('.mobile-menu').fadeToggle();
+    $('.mobile-menu').fadeToggle();
 });
 
 $('.btn-close').on('click', function () {
@@ -28,9 +28,44 @@ $('.down').on("click", function () {
     $input.change();
     return false;
 });
-$('.up').on("click",function () {
+$('.up').on("click", function () {
     let $input = $(this).parent().find('input');
     $input.val(parseInt($input.val()) + 1);
     $input.change();
     return false;
+});
+
+$('.home-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#right"></use></svg></button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#left"></use></svg></button>'
+});
+
+$('.user-feedback-slider').slick({
+    slidesToShow: 3,
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#right"></use></svg></button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#left"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+    ]
+});
+
+$('.blog-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    arrows: false,
+    dots: true
 });
